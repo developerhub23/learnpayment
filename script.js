3957c2570dbc182049eb6adac3d407699a9c7ef7
developerhub <anishdey0602@gmail.com>
2026-07-12 21:35:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-document.getElementById("payBtn").onclick = async function () {
+async function payNow() {
 
-    alert("Next step: We'll fetch an Order ID from our backend.");
+    const response = await fetch(
+        "YOUR_WORKER_URL"
+    );
 
-};
+    const order = await response.json();
+
+    const options = {
+
+        key: "YOUR_TEST_KEY_ID",
+
+        amount: order.amount,
+
+        currency: order.currency,
+
+        name: "My Website",
+
+        description: "₹1 Test Payment",
+
+        order_id: order.id,
+
+        handler: function (response) {
+
+            alert(
+                "Payment Successful!\n\n" +
+                "Payment ID: " + response.razorpay_payment_id
+            );
+
+            console.log(response);
+
+        }
+
+    };
+
+    const rzp = new Razorpay(options);
+
+    rzp.open();
+
+}
