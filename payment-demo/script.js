e038cb6eca6c9e94fda6901985c5fd04bf36461c
developerhub <anishdey0602@gmail.com>
2026-07-12 20:35:02 +0530

    Update script.js

--- payment-demo/script.js
+++ payment-demo/script.js
@@ -3,7 +3,7 @@ function generateQR(){
 document.getElementById("qrcode").innerHTML="";
 
 const upi =
-"upi://pay?pa=YOUR_UPI_ID@bank&pn=Rishi&am=1&cu=INR&tn=Demo Payment";
+"upi://pay?pa=8093298778-2@superyes8093298778-2@superyes&pn=Rishi&am=1&cu=INR&tn=Demo Payment";
 
 new QRCode(document.getElementById("qrcode"),{
 
