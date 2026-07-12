76dbd9102d801691d0ec8c98a61351ed10391e6f
developerhub <anishdey0602@gmail.com>
2026-07-12 21:08:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-function generateQR(){
+document.getElementById("payBtn").onclick = async function () {
 
-document.getElementById("qrcode").innerHTML="";
+    alert("Next step: We'll fetch an Order ID from our backend.");
 
-const upi =
-"upi://pay?pa=8093298778-2@superyes8093298778-2@superyes&pn=Rishi&am=1&cu=INR&tn=Demo Payment";
-
-new QRCode(document.getElementById("qrcode"),{
-
-text:upi,
-width:250,
-height:250
-
-});
-
-}
+};
